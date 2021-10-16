2d648cd202714f5cae70f356dfb25b2b6f7133b5
rebecalvarezc <realce@alumni.uv.es>
2021-10-16 11:09:57 +0200

    added html, streamlit and constants

--- app.py
+++ app.py
@@ -2,11 +2,51 @@
 import time
 from pprint import pprint
 import requests
+import streamlit as st
 
 # API from openweatherapp.com
 api_key = 'e8a6318f847fcff8956b223b10519dff'
 
+# HTML
+html_template = """
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <div style = "background-color: {}; padding: 10px; border-radius: 10px">
+    <h1 style = "color: {}; text-align:center;font-family:'Roboto', serif;"> My friends weather channel </h1>
+    </div>"""
+html_intro = """
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <div style = "background-color: {}; padding: 5px; border-radius: 10px">
+    <h2 style = "color: {}; text-align:center; font-size: 32px; font-family:'Roboto', serif;"> {} </h2>
+    <h4 style = "color: {}; text-align:center; font-size: 16px; font-family:'Roboto', sans serif;"> {} </h4>
+    <br>
+    <p style = "font-family:'Roboto', serif; padding: 8px"> {} </p>
+    </div>"""
+html_boxes = """
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+    <div style = "background-color: {}; padding: 5px; border-radius: 10px">
+    <img src="" width= "70px" height= "50px" align="{}"/>
+    <h4 style = "color: {}; text-align:left; font-size: 16px; font-family:'Roboto', sans serif;"> {} </h4>
+    <p style = "font-family:'Roboto', serif; padding: 8px"> {} </p>
+    </div>"""
 
+# Constants
+
+HISTORY = '''My name is Rebeca Alvarez and I am originally from Venezuela. I moved 4 years ago to Valencia, Spain.
+As i have a lor of friends around the world, I decided to create this app to check the weather wherever they are. 
+Down below I will introduce you to myy friends, I hope you like them as much as I do :)'''
+
+VALERIA = {
+    'name': 'Valeria',
+    'home': '''Valeria is my childhood best friend! She is married to Carlos and they both live happily in Dublin, Ireland.
+    I have visited her twice already and I really love how beautiful Ireland is and how lovely the people in there are. 
+    But Ireland is really rainy, so I constantly worry that she might get sick.'''}
+
+
+# Functions
 def get_data():
     city = 'Valencia'
     endpoint = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
@@ -21,6 +61,7 @@ def design(condition):
     :param condition:
     :return:
     """
+
     if condition == 'clear sky':
         pass
     elif condition == 'few clouds':
@@ -53,8 +94,11 @@ def get_weather_data():
     timezone = data['timezone'] / 3600
     sunset = time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunset']))
 
-    pprint(sunset)
+    pprint(data)
 
 
 if __name__ == '__main__':
     get_weather_data()
+    st.markdown(html_template.format('#955251', 'white'), unsafe_allow_html=True)
+    st.markdown(html_intro.format('white', '#955251', 'Welcome!', 'black', 'This is my personal app that I made for my friends :)', HISTORY), unsafe_allow_html=True)
+    st.markdown(html_boxes.format('white','left', '#955251', VALERIA['name'], VALERIA['home']), unsafe_allow_html=True)
