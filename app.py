a53ce0fd7bfa997c369ef7b69b0f17cc3ff4c3fe
rebecalvarezc <realce@alumni.uv.es>
2021-10-16 10:31:49 +0200

    extracted all the data parameters

--- app.py
+++ app.py
@@ -8,43 +8,52 @@ api_key = 'e8a6318f847fcff8956b223b10519dff'
 
 
 def get_data():
-    city = 'Puerto Rico'
+    city = 'Valencia'
     endpoint = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
     r = requests.get(endpoint)
     if r.status_code in range(200, 300):
         return r.json()
 
 
-# canvas = tk.Tk()
-# canvas.geometry('600x500')
-# canvas.title('Weather App')
-#
-# f = ('poppins', 15, 'bold')
-# t = ('poppins', 35, 'bold')
-#
-# textfield = tk.Entry(canvas, font=t)
-# textfield.pack(pady=20)
-# textfield.focus()
-#
-# label1 = tk.label(canvas, font=t)
-# label1.pack()
-# label2 = tk.label(canvas, font=f)
-# label2.pack()
-#
-# canvas.mainloop()
+def design(condition):
+    """
+    This function defines the page design according to the weather condition.
+    :param condition:
+    :return:
+    """
+    if condition == 'clear sky':
+        pass
+    elif condition == 'few clouds':
+        pass
+    elif condition == 'scattered clouds':
+        pass
+    elif condition == 'broken clouds':
+        pass
+    elif condition == 'shower rain':
+        pass
+    elif condition == 'rain':
+        pass
+    elif condition == 'thunderstorm':
+        pass
+    elif condition == 'snow':
+        pass
+    elif condition == 'mist':
+        pass
+
+
 def get_weather_data():
     data = get_data()
-    condition = data['weather'][0]['main']
+    condition = data['weather'][0]['main'].lower()
     temp = round(data['main']['temp'] - 273.15)  # En qué unidades? -> Kelvin -> Celsius
     min_temp = round(data['main']['temp_min'] - 273.15)
     max_temp = round(data['main']['temp_max'] - 273.15)
     humidiy = round(data['main']['humidity'], 2)  # %
     wind_speed = data['wind']['speed']  # m/s
-    sunrise = time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunrise'])) # GMT+1 London & Spain = GMT+2
-    timezone = data['timezone']/3600
-    sunset = data['sys']['sunset'] # UTC
+    sunrise = time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunrise']))
+    timezone = data['timezone'] / 3600
+    sunset = time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunset']))
 
-    pprint(data)
+    pprint(sunset)
 
 
 if __name__ == '__main__':
