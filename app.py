6098a0f69d1670242836cb9e27d767d59f7feae9
rebecalvarezc <realce@alumni.uv.es>
2021-10-17 13:19:10 +0200

    added while cicle & fixed the time showed

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # import tkinter as tk
+import datetime
 import time
 from pprint import pprint
 import requests
 import streamlit as st
 
 # API from openweatherapp.com
-api_key = 'e8a6318f847fcff8956b223b10519dff'
+api_key = ''
 
 
 # Functions
@@ -31,16 +32,36 @@ def get_weather_data(city: str) -> dict:
     data = get_data(city)
     conditions = {
         'city': city,
+        'country': data['sys']['country'],
         'condition': data['weather'][0]['main'],
         'temp': round(data['main']['temp'] - 273.15),
         'min_temp': round(data['main']['temp_min'] - 273.15),
         'max_temp': round(data['main']['temp_max'] - 273.15),
         'humidity': round(data['main']['humidity'], 2),  # %
         'wind_speed': data['wind']['speed'],
-        'sunrise': time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunrise'])),
-        'timezone': data['timezone'] / 3600,
-        'sunset': time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunset']))
+        'sunrise': time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunrise'] + data['timezone'])),
+        'sunset': time.strftime('%A %I:%M%p', time.gmtime(data['sys']['sunset'] + data['timezone']))
     }
     return conditions
 
 
+def share_data():
+    date = datetime.date.today().strftime('%A %d of %B')
+    while True:
+        city = input('Please write the name of the city in where you want to know the weather.\n--> ').lower().title()
+        json_data = get_data(city)
+        if json_data:
+            conditions = get_weather_data(city)
+            print(
+                f"""\nToday, {date}, in {conditions['city']} ({conditions['country']}) the temperature will be {conditions['temp']}ºC.
+    The percentage of humidity will be: {conditions['humidity']}%
+    The max temperature today will be: {conditions['max_temp']}ºC 
+    The min temperatura will be: {conditions['min_temp']} ºC.
+    The sun will rise at {conditions['sunrise']} and set at: {conditions['sunset']}\n""")
+        else:
+            print(f'Sorry, no data was found for {city}')
+            break
+
+
+if __name__ == '__main__':
+    share_data()
